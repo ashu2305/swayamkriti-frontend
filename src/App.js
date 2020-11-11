616a654056659a95846fe5131a8735ef05616257
Shashwat-Sahu <shashwatsahu25@gmail.com>
2020-11-11 12:53:02 +0530

    forgotpassword

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Signin from './Components/Login/login'
 import Signup from './Components/Signup/signup'
+import ForgotPassword from './Components/ForgotPassword/ForgotPassword'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
     <Switch>
       <Route path="/login" component={Signin}/>
       <Route path="/signup" component={Signup}/>
-
+      <Route path="/forgotpassword" component={ForgotPassword}/>
     </Switch>
     </BrowserRouter>
   );
--- src/Components/ForgotPassword/ForgotPassword.js
+++ src/Components/ForgotPassword/ForgotPassword.js
@@ -1,36 +1,24 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import Particles from "react-particles-js"
-import "./signup.css"
+import "./forgotpassword.css"
 
 const Signup = () => {
-    return (<div className="signupBackground">
+    return (<div className="forgotpasswordBackground">
         <Particles className="particleBackground" />
         <div className="container">
             <div className="row">
                 <div className="col s12 m12">
                     <div className="col s12 offset-m3 m6">
-                        <div class="card signupBox">
+                        <div class="card forgotpasswordBox">
                             <div class="card-content white-text center-align">
                                 
-                                <h1 className="signupHeading">Sign Up</h1>
-                                <div class="input-field">
-                                    <input id="name" type="text" class="validate white-text" />
-                                    <label for="name">Name</label>
-                                </div>
+                                <h1 className="forgotpasswordHeading">Forgot Password</h1>
                                 <div class="input-field">
                                     <input id="email" type="email" class="validate white-text" />
                                     <label for="email">Email</label>
                                 </div>
-                                <div class="input-field">
-                                    <input id="password" type="password" class="validate white-text" />
-                                    <label for="password">Password</label>
-                                </div>
-                                <div class="input-field">
-                                    <input id="rollNo" type="text" class="validate white-text" />
-                                    <label for="rollNo">Roll No.</label>
-                                </div>
-                                <button class="btn waves-effect waves-light" type="submit" name="action">Sign Up
+                                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
                                     <i class="material-icons right">
                                         send
                                     </i>
@@ -38,7 +26,6 @@ const Signup = () => {
                             </div>
                             <div class="card-action center-align">
                                 
-                                <Link to="/login" className="white-text">Already have an account</Link>
                             </div>
                         </div>
                     </div>
